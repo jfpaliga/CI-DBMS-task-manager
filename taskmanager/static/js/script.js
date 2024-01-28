69f8af8b73a57b9305dac0c2e4eb3942fc5979c6
jfpaliga <>
2024-01-28 13:39:32 +0000

    Step eight - add functionality to add task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -5,6 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
+    let delete_modal = document.querySelectorAll('.modal');
+    M.Modal.init(delete_modal);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
